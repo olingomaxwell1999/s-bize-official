8e980c6321e405648ff3f761aa392481cb3e63e1
Olingo Maxwell <olingomaxwell1999@gmail.com>
2022-09-21 17:12:48 +0300

    Sauti africa video update

--- src/pages/Sautiafrica.jsx
+++ src/pages/Sautiafrica.jsx
@@ -13,7 +13,7 @@ function Sautiafrica() {
 
       <Displayfive title="Sauti Africa" desc="Sauti Afrika is a community based organization that supports young people to use art as a way to create social change in the communities. The organization was established in 2016 and seeks to create socioeconomic transformation for communities by incorporating innovative ways using integrated right-based approaches in Health, Education, Economic Empowerment and Leadership and Governance. Sauti Africa utilizes a multi-sectoral approach, Quadra Helix development approach bringing together Government, Private sector, Academia and Civil society.  This response has allowed for implementation of programs in Mental and Sexual Health, Education, Economic Empowerment and Leadership and Governance Sectors. The organization engages young people in a constructive manner enabling them to initiate and build their ideas, develop prototypes, test and develop products that meet the people’s needs and help reduce the problems and challenges that are most faced in the community."/>
 
-      <Displaysix id='CNiJUGnDo04'/>
+      <Displaysix id='hIK2ffbPbZk'/>
 
       <Displayseven title='suggested project & deliverable' desc="A Million Comforts: This program aims to provide affordable pads to young women in Mukuru. The program seeks to eradicate period poverty by offering Affordable, Biodegradable and Super Absorbent Banana Fiber Sanitary towels. With utilization of Banana Pseudo stem, an agricultural waste for textile, Sauti Africa manufactures pads that are eight times more absorbent than any other pad in the market by using non-woven organic fiber and non-woven organic clothes to keep girls in villages and informal settlements in school and create jobs for local women.
  
