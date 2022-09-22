1cdf2ba70079c80340974cf797c7175566d79a46
Olingo Maxwell <olingomaxwell1999@gmail.com>
2022-09-22 10:49:54 +0300

    Font size update
    Increased  paragraph size

--- src/Components/Copyright/Copyright.jsx
+++ src/Components/Copyright/Copyright.jsx
@@ -5,7 +5,9 @@ import './copyright.scss'
 function Copyright() {
   return (
     <div className='copy'>
-        <span>Copyright &copy; 2022 <a href="https://www.nairobits.com">Nairobits Trust.</a>  All Rights Reserved</span>
+        <span>Copyright &copy; 2022 <a href="https://www.nairobits.com" target="_blank" rel="noreferrer">Nairobits Trust.</a>  All Rights Reserved</span>
+
+        <span><a className='max' href="https://www.linkedin.com/in/olingo-maxwell-web-developer/" target="_blank" rel="noreferrer">olingo</a></span>
     </div>
   )
 }
