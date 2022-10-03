cbcf1ef59cbbb93a26a5cdff30f6e316375824fc
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2022-10-03 17:40:45 +0300

    update video for Action Foundation
    new video

--- src/pages/ActionFoundation.jsx
+++ src/pages/ActionFoundation.jsx
@@ -13,7 +13,7 @@ function ActionFoundation() {
 
       <Displayfive title="About The Action Foundation" desc="The Action Foundation (TAF) is a Kenyan community-driven organization that works to end exclusion, discrimination, and violence against disabled children, women, and girls in marginalized communities. The organization works to reach those furthest from necessary services and opportunities by working with their caregivers and the community around them. This enables these vulnerable groups to improve their lives, strengthen their resilience, and cultivate long-term change. TAF strives to create inclusive and resilient communities where children, women, and girls with disabilities can thrive. The organization envisions a fully inclusive Africa in which disabled children, women, and girls can reach their full potential. Over the years, TAF has adapted its interventions to beneficiary needs through community engagement, innovation, and co-creation of activities with target groups."/>
 
-      <Displaysix id='XHIP1x3f_Z0'/>
+      <Displaysix id='ODSlkN8Ziok'/>
 
       <Displayseven title='suggested project & deliverable' desc="Paa is a social enterprise that is run separately from The Action Foundation. It focuses on providing branding services to businesses, SMEs, and other organizations.  The organization is working to stabilize the business in order to financially support programs run by The Action Foundation" link="https://theactionfoundationkenya.org/" />
     </div>
